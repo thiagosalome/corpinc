80c9f119668d0ddad8e6780e58a6c23e88f6c6d7
thiagosalome <thiagosalome@gmail.com>
2019-03-04 20:58:13 -0300

    Styling home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,9 +9,14 @@ const Home = () => {
     <main className="home">
       <Header></Header>
       <section className="home__card">
-        <div className="home__slider js-home-slider">
-          <FormSignup></FormSignup>
-          <FormLogin></FormLogin>
+        <div className="home__card-image">
+          <h2><strong>Venha conhecer</strong><br/>nossa aplicação!</h2>
+        </div>
+        <div className="home__card-slider-wrapper">
+          <div className="home__card-slider">
+            <FormSignup></FormSignup>
+            <FormLogin></FormLogin>
+          </div>
         </div>
       </section>
     </main>
