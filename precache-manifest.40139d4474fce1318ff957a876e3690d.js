f30fdcbb0c399d15287604b7e814d34c486d5cb2
thiagosalome <thiagosalome@gmail.com>
2019-03-08 03:32:16 -0300

    Updates

---
+++ precache-manifest.40139d4474fce1318ff957a876e3690d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd0e8f7451f56ba877b2aa56eb13f5fb",
+    "url": "./static/media/home.cd0e8f74.jpeg"
+  },
+  {
+    "revision": "0330284f237b2a7358b4034089bfc56e",
+    "url": "./static/media/home-card.0330284f.svg"
+  },
+  {
+    "revision": "aa76c6ef767965dddcf2a724874097d1",
+    "url": "./static/media/banner.aa76c6ef.png"
+  },
+  {
+    "revision": "8042c89c4007f373920db51753e5301e",
+    "url": "./static/media/banner-mobile.8042c89c.png"
+  },
+  {
+    "revision": "fa0b832c8f6919076e2b1ae32fcd48a5",
+    "url": "./static/media/404.fa0b832c.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "f8fe81e4bdaf31df8a43",
+    "url": "./static/js/main.f8fe81e4.chunk.js"
+  },
+  {
+    "revision": "0f626b6253a25c8c4f33",
+    "url": "./static/js/2.0f626b62.chunk.js"
+  },
+  {
+    "revision": "f8fe81e4bdaf31df8a43",
+    "url": "./static/css/main.be0e47a8.chunk.css"
+  },
+  {
+    "revision": "1710a0a74b5f8df4b00706fefb777447",
+    "url": "./index.html"
+  }
+];
