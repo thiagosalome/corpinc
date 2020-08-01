134ba9adc16eab424289f41c2be077c49c48b7f1
Thiago Salome <thiagosalome@gmail.com>
2020-08-01 16:36:41 -0300

    Corrigindo eslint do componente de rotas

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,26 @@
 module.exports = {
-  extends: 'airbnb',
   env: {
-    browser: true
+    browser: true,
+    es2020: true,
   },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
   rules: {
-    'jsx-a11y/label-has-associated-control': 'off'
-    'radix': 'off'
-  }
+    'no-param-reassign': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    radix: 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
 };
