3219b0a4497ca7e1468681518919daacae28c77e
Thiago Salome <thiagosalome@gmail.com>
2020-08-02 14:08:09 -0300

    Updates

---
+++ precache-manifest.a16a7ed2769c82150afb9f744acc3750.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5132b5af1a711ae1f0c8082a6a4a5066",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3a8905dc597666f3cc78",
+    "url": "/todo-list/static/css/main.0e7ecea6.chunk.css"
+  },
+  {
+    "revision": "6914f39e090292c6c10e",
+    "url": "/todo-list/static/js/2.566c4cdc.chunk.js"
+  },
+  {
+    "revision": "f550e844aab176aa122a79202de72287",
+    "url": "/todo-list/static/js/2.566c4cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8905dc597666f3cc78",
+    "url": "/todo-list/static/js/main.8e04750e.chunk.js"
+  },
+  {
+    "revision": "8c42f74df1f0e8c5c6ce",
+    "url": "/todo-list/static/js/runtime-main.9f011628.js"
+  },
+  {
+    "revision": "fa0b832c8f6919076e2b1ae32fcd48a5",
+    "url": "/todo-list/static/media/404.fa0b832c.png"
+  },
+  {
+    "revision": "8042c89c4007f373920db51753e5301e",
+    "url": "/todo-list/static/media/banner-mobile.8042c89c.png"
+  },
+  {
+    "revision": "aa76c6ef767965dddcf2a724874097d1",
+    "url": "/todo-list/static/media/banner.aa76c6ef.png"
+  },
+  {
+    "revision": "0330284f237b2a7358b4034089bfc56e",
+    "url": "/todo-list/static/media/home-card.0330284f.svg"
+  },
+  {
+    "revision": "cd0e8f7451f56ba877b2aa56eb13f5fb",
+    "url": "/todo-list/static/media/home.cd0e8f74.jpeg"
+  },
+  {
+    "revision": "659787288f1824921c38dcf4d3158768",
+    "url": "/todo-list/static/media/loading.65978728.gif"
+  }
+]);
